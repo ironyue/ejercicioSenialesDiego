9c6cff45f2324b9ba3b23abb3b08225afca79a3c
ironyue <yue68@ymail.com>
2021-04-22 18:57:10 -0300

    generate_block

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -8,4 +8,9 @@ func _ready():
 
 
 func _on_RedGenerator_button_down():
+	generate_block()
 	pass # Replace with function body.
+
+
+func generate_block():
+	var ins_rojo_chico = load("res://scenes/Blocks/Red/BlockRed.tscn")
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -10,7 +10,10 @@ extends TextureButton
 func _ready():
 	pass # Replace with function body.
 
+func generate_block():
+	var ins_rojo_chico = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_VioletGenerator_button_down():
+	generate_block()
+	pass # Replace with function body.
